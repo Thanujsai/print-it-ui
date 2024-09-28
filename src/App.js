85f060e7c984b9a5f7f74898df9c387e860abc16
thanujsai <sboda@modeln.com>
2024-09-28 19:17:28 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ import Home from './components/Home';
 import ProductShowcase from './components/ProductShowcase';
 import Footer from './components/Footer';
 import './css/BackgroundVideoStyles.css'; // Import your CSS file
+import UploadPage from './components/UploadPage';
 
 function App() {
   return (
     <Router>
       <div className="app">
-      <video className="background-video" autoPlay muted loop>
-        <source src="/src-videos/background.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/bikes" element={<ProductShowcase />} />
+            <Route path="/models" element={<ProductShowcase />} />
+            <Route path="/upload" element={<UploadPage />} />
             {/* Add more routes as needed */}
           </Routes>
         </main>
---
+++ src/components/UploadPage.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Header from './Header';
+import '../css/UploadPage.css';
+import { useNavigate } from 'react-router-dom';
+
+const UploadPage = () => {
+  const [file, setFile] = useState(null);
+
+  const navigate = useNavigate();
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append('image', file);  // Note the change here
+
+    fetch('http://localhost:5001/api/upload-image', {  // Updated endpoint
+      method: 'POST',
+      body: formData,
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.success) {
+          console.log('Success:', data);
+          alert('File uploaded successfully!');
+        } else {
+          alert('File upload failed!');
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        alert('File upload failed!');
+      });
+  };
+
+  const handleBack = () => {
+    navigate("/")
+  }
+
+  return (
+    <MyComponent>
+      <Header />
+      <Container>
+        <Title>Upload Your 3D Model</Title>
+        <Input type="file" accept=".stl,.obj,.fbx,.jpg" onChange={handleFileChange} />
+        <div className='button-container'>
+        <Button onClick={handleUpload}>Upload</Button>
+        <Button onClick={handleBack}>Back</Button>
+        </div>
+      </Container>
+    </MyComponent>
+  );
+};
+
+export default UploadPage;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 20px;
+`;
+
+const Input = styled.input`
+  margin: 10px 0;
+`;
+
+const Button = styled.button`
+  margin-top: 20px;
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #FFFFFF;
+    color: #00008B;
+  }
+`;
+
+const MyComponent = styled.div`
+  background-image: url('/src-images/background.jpg');
+  background-size: cover; /* Ensures the image covers the entire component */
+  background-position: center; /* Centers the image */
+  background-repeat: no-repeat; /* Prevents the image from repeating */
+  width: 100vw; /* Full width of the viewport */
+  height: 100vh; /* Full height of the viewport */
+  padding: 20px;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
