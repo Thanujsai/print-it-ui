8859f0ef75d32c13321ff4383beb6209e63f6b1c
thanujsai <sboda@modeln.com>
2025-02-13 17:55:06 +0530

    keyframes

--- src/components/FramerMotion/ExitAndEnterAnimation.js
+++ src/components/FramerMotion/ExitAndEnterAnimation.js
@@ -19,7 +19,12 @@ function ExitAndEnterAnimation() {
             {removeInnerCircle === false && (
             <motion.div 
                 initial={{opacity: 0, y:0}}
-                animate={{opacity: 1, y:40}}
+                animate={{opacity: 1, 
+                    y:40,
+                    scale: [null,1.2,0.5,0.1,1],//keyframes are scaled to 1.2, 1.5, 1.1 and 1
+                    backgroundColor: [null, "#111111","#eb4034","#86EFAC"],//also changing background colors while scaling
+                    borderRadius: [null, "40%", "10%", "100%"],
+                }}
                 transition={{duration: 10}}
                 className="bg-green-300 h-24 w-24 rounded-full"
                 exit={{opacity: 0, y: 1000, transition: {duration: 20}}}>
